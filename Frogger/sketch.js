b3510ee576c961200325f21310a66bb36f54e90f
Jeppe Veirum Larsen <veirum@gmail.com>
2020-04-02 14:13:22 +0200

    Frogger Class
    Created a grog class

--- Frogger/sketch.js
+++ Frogger/sketch.js
@@ -1,9 +1,8 @@
 function setup() {
-  // put setup code here
-  createCanvas(800,600);
+  createCanvas(800, 600);
 }
 
 function draw() {
-  // put drawing code here
-  background(128);
+  background(0);
+
 }
